afa737db8f0e0290adde4b3ca0d2844a470728b8
Jesusbs20 <74325046+Jesusbs20@users.noreply.github.com>
2024-04-28 18:26:43 -0500

    Add files via upload

--- src/Components/Dashboard/Contador.jsx
+++ src/Components/Dashboard/Contador.jsx
@@ -1,74 +1,92 @@
-import React, { useState } from 'react';
-import imagen from '../../LoginAssets/ImagenMAG.jpg'
-import Calendar from 'react-calendar'; 
-import './Gerente.scss'; 
+import React, { useState } from "react";
+import imagen from "../../LoginAssets/ImagenMAG.jpg";
+import upload from "../../LoginAssets/Upload.png";
+import Calendar from "react-calendar";
+import "./Gerente.scss";
 
 const Gerente = () => {
-  const [selectedDate, setSelectedDate] = useState(new Date()); 
+  const [selectedDate, setSelectedDate] = useState(new Date());
 
   return (
     <div className="gerente-container">
-      <div className="company-image">
-        {/* Company image */}
-        <img src={imagen} alt="Company Logo" className="small-logo" />
-      </div>
-      <div className="left-section">
-        {/* Table for individual performance */}
-        <h2>Porcentaje de cumplimiento individual</h2>
-        <table className="individual-performance">
-          <thead>
-            <tr>
-              <th>Cumplió</th>
-              <th>Contador asignado</th>
-              <th>Compañías</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td><input type="checkbox" /></td>
-              <td>Pepito Perez</td>
-              <td>Compañía A</td>
-            </tr>
-            {/* Add more rows */}
-          </tbody>
-        </table>
-        <h2>Porcentaje de cumplimiento</h2>
-        <div className="percentage-section">
-          <table className="percentage-table">
+      <header className="header">
+        <div className="company-image">
+          {/* Company image */}
+          <img src={imagen} alt="Company Logo" className="small-logo" />
+        </div>
+        <div className="header-title">
+          <h2>Interface Contador</h2>
+        </div>
+      </header>
+      <main className="main">
+        <section className="left-section">
+          <h2>Porcentaje de cumplimiento individual</h2>
+          <table className="individual-performance">
             <thead>
               <tr>
-                <th>Contador</th>
-                <th>Porcentaje</th>
+                <th>Cumplió</th>
+                <th>Contador asignado</th>
+                <th>Compañías</th>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <td>Contador 1</td>
                 <td>
-                  <div className="percentage-bar">
-                    <div className="fill" style={{ width: '75%' }}></div>
-                  </div>
+                  <input type="checkbox" />
                 </td>
+                <td>Pepito Perez</td>
+                <td>Compañía A</td>
               </tr>
-              {/* Add more counters */}
+              {/* Add more rows */}
             </tbody>
           </table>
-        </div>
-      </div>
-      <div className="right-section">
-        <div className="calendar-section">
-          <h2>Fecha de entregas importantes</h2>
-          <Calendar onChange={setSelectedDate} value={selectedDate} />
-        </div>
-        <div className="file-upload-section">
-          <h2>Carga de archivos para aprobación</h2>
-          <div className="file-upload-buttons">
-            <button>Cargar Archivos</button>
-            <button>Enviar</button>
-            <button>Descargar Base</button>
+          <h2>Porcentaje de cumplimiento</h2>
+          <div className="percentage-section">
+            <table className="percentage-table">
+              <thead>
+                <tr>
+                  <th>Contador</th>
+                  <th>Porcentaje</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Contador 1</td>
+                  <td>
+                    <div className="percentage-bar">
+                      <div className="fill" style={{ width: "75%" }}></div>
+                    </div>
+                  </td>
+                </tr>
+                {/* Add more counters */}
+              </tbody>
+            </table>
           </div>
-        </div>
-      </div>
+        </section>
+        <section className="right-section">
+          <div className="calendar-section">
+            <h2>Fecha de entregas importantes</h2>
+            <Calendar
+              onChange={setSelectedDate}
+              value={selectedDate}
+              calendarContainerClassName="small-calendar"
+            />
+          </div>
+          <div className="file-upload-section">
+            <h2>Carga de archivos para aprobación</h2>
+            <div className="upload-image">
+              <button>
+                <img src={upload} alt="Upload Logo" className="small-logo" />
+              </button>
+            </div>
+            <div className="file-upload-buttons">
+              <button>Cargar Archivos</button>
+              <button>Enviar</button>
+              <button>Descargar Base</button>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   );
 };
